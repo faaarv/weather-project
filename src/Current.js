3497426590a17048f0fc3a71c57859da7fe4eba1
fafa <112162769+f5623@users.noreply.github.com>
2023-01-10 14:01:26 +0330

    adding icons

--- src/Current.js
+++ src/Current.js
@@ -1,27 +1,43 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./App.css"
+import { BsDropletHalf } from "react-icons/bs";
+import { BsWind } from "react-icons/bs";
+import { IconContext } from "react-icons";
+import { WiThermometer } from "react-icons/wi";
+
+
+
 let days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
 
 export default function Current(props) {
     return (
         <div className="weather-now mt-1">
                 <div className="row">
-                    <div className="col-4 pt-3 align-middle">
-                        <p className='fs-2 text-capitalize'>{props.input.city}</p>
-                        <p className="text-muted">
+                    <div className="col-4 pt-3 ps-sm-3 align-middle text-start">
+                        <p className='fs-1 text-capitalize '>{props.input.city}</p>
+                        <p className="text-muted ps-1">
                         <span>{days[new Date().getDay()]}</span>, 
                         <span className="day">{new Date().toLocaleString('en-GB', {day: 'numeric',
-                        month: 'short',}) + ""}</span> ,<span className="time">
+                        month: 'short',}) + ""}</span>
+                        < br/>
+                        <span className="time ">
                         {new Date().toLocaleString('en-US', {hour: 'numeric',minute: 'numeric',hour12: true,})}</span>
                         </p>
                     </div>
-                    <div className="col-4 align-middle pt-4">
-                        <span>Temperature: {props.input.currentTemp}°C</span>
-                        <img src={props.input.imgSrc} alt="weather icon"/>
+                    <div className="col-4 pt-3 align-middle">
+                    
+                        <span>
+                        <IconContext.Provider value={{ color: " black", size: "3em" , className: "global-class-name" }}>
+                            <div>
+                            <WiThermometer />{props.input.currentTemp}°C<img src={props.input.imgSrc} alt="weather icon"/>
+                            </div>
+                        </IconContext.Provider></span>
+                        
                     </div>
+
                     <div className="col-4 align-middle pt-3 text-center">                   
-                        <p>WindSpeed : {props.input.windSpeed}km/h</p>
-                        <p>Humidity : {props.input.humidity}%</p>
+                        <p><BsWind /> {props.input.windSpeed}km/h</p>
+                        <p><BsDropletHalf /> {props.input.humidity}%</p>
                         <p> {props.input.weatherDesc}</p>
                     </div>
                 </div>
