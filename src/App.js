b1c27cf927d9f8188d520b010793cde9d5e6faee
arv <aerofaesk@proton.me>
2023-05-12 15:00:34 -0600

    edit links and read me

--- src/App.js
+++ src/App.js
@@ -13,8 +13,8 @@ export default function App() {
 
      </div>
      <small className="footnote">
-      <a href="https://github.com/f5623/react-project">This project </a>
-      is coded by <a href="https://github.com/f5623">Fafa</a> as
+      <a href="https://github.com/faaarv/weather-project">This project </a>
+      is coded by <a href="https://github.com/faaarv">Fafa</a> as
       <a href="https://www.shecodes.io/"> SheCodes </a> React workshop final project.
      </small>
     </div>
